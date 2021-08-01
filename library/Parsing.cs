a21acea47576647b419c24cb49da8ee0b913ae57
Mårten Åsberg <marten.asberg@outlook.com>
2021-08-01 16:18:47 +0200

    Pattern parsing library
    Can parse up to four values. Is that enough?

---
+++ library/Parsing.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using static AoC.Library.Functional;
+
+namespace AoC.Library
+{
+	public static class Parsing
+	{
+		public static Func<string, T?> PatternParser<T>(string pattern) =>
+			PatternParser<T, T>(new Regex(pattern), Id);
+		public static Func<string, TO?> PatternParser<TI, TO>(string pattern, Func<TI, TO> transformer) =>
+			PatternParser<TI, TO>(new Regex(pattern), transformer);
+		public static Func<string, T?> PatternParser<T>(Regex pattern) =>
+			PatternParser<T, T>(pattern, Id);
+		public static Func<string, TO?> PatternParser<TI, TO>(Regex pattern, Func<TI, TO> transformer)
+		{
+			if (pattern.GetGroupNumbers().Length != 2)
+			{
+				throw new ArgumentException("Pattern must have exactly one group.", nameof(pattern));
+			}
+
+			return input =>
+			{
+				Match match = pattern.Match(input);
+				if (match.Success && match.Groups[1].Success)
+				{
+					if (Convert.ChangeType(match.Groups[1].Value, GetType<TI>(), CultureInfo.InvariantCulture) is TI value)
+					{
+						return transformer(value);
+					}
+					else
+					{
+						throw new FormatException($"Could not convert the group (\"{match.Groups[1].Value}\") to {GetType<TI>().Name}. From \"{input}\".");
+					}
+				}
+				else
+				{
+					return default;
+				}
+			};
+		}
+
+		public static Func<string, (T1, T2)?> PatternParser<T1, T2>(string pattern) =>
+			PatternParser<T1, T2, (T1, T2)?>(new Regex(pattern), static (v1, v2) => (v1, v2));
+		public static Func<string, TO?> PatternParser<T1, T2, TO>(string pattern, Func<T1, T2, TO> combiner) =>
+			PatternParser<T1, T2, TO>(new Regex(pattern), combiner);
+		public static Func<string, (T1, T2)?> PatternParser<T1, T2>(Regex pattern) =>
+			PatternParser<T1, T2, (T1, T2)?>(pattern, static (v1, v2) => (v1, v2));
+		public static Func<string, TO?> PatternParser<T1, T2, TO>(Regex pattern, Func<T1, T2, TO> combiner)
+		{
+			if (pattern.GetGroupNumbers().Length != 3)
+			{
+				throw new ArgumentException("Pattern must have exactly two groups.", nameof(pattern));
+			}
+
+			return input =>
+			{
+				Match match = pattern.Match(input);
+				if (match.Success && match.Groups[1].Success && match.Groups[2].Success)
+				{
+					if (Convert.ChangeType(match.Groups[1].Value, GetType<T1>(), CultureInfo.InvariantCulture) is T1 value1)
+					{
+						if (Convert.ChangeType(match.Groups[2].Value, GetType<T2>(), CultureInfo.InvariantCulture) is T2 value2)
+						{
+							return combiner(value1, value2);
+						}
+						else
+						{
+							throw new FormatException($"Could not convert group 2 (\"{match.Groups[2].Value}\") to {GetType<T2>().Name}. From \"{input}\".");
+						}
+					}
+					else
+					{
+						throw new FormatException($"Could not convert group 1 (\"{match.Groups[1].Value}\") to {GetType<T1>().Name}. From \"{input}\".");
+					}
+				}
+				else
+				{
+					return default;
+				}
+			};
+		}
+
+		public static Func<string, (T1, T2, T3)?> PatternParser<T1, T2, T3>(string pattern) =>
+			PatternParser<T1, T2, T3, (T1, T2, T3)?>(new Regex(pattern), static (v1, v2, v3) => (v1, v2, v3));
+		public static Func<string, TO?> PatternParser<T1, T2, T3, TO>(string pattern, Func<T1, T2, T3, TO> combiner) =>
+			PatternParser<T1, T2, T3, TO>(new Regex(pattern), combiner);
+		public static Func<string, (T1, T2, T3)?> PatternParser<T1, T2, T3>(Regex pattern) =>
+			PatternParser<T1, T2, T3, (T1, T2, T3)?>(pattern, static (v1, v2, v3) => (v1, v2, v3));
+		public static Func<string, TO?> PatternParser<T1, T2, T3, TO>(Regex pattern, Func<T1, T2, T3, TO> combiner)
+		{
+			if (pattern.GetGroupNumbers().Length != 4)
+			{
+				throw new ArgumentException("Pattern must have exactly three groups.", nameof(pattern));
+			}
+
+			return input =>
+			{
+				Match match = pattern.Match(input);
+				if (match.Success && match.Groups[1].Success && match.Groups[2].Success &&
+					match.Groups[3].Success)
+				{
+					if (Convert.ChangeType(match.Groups[1].Value, GetType<T1>(), CultureInfo.InvariantCulture) is T1 value1)
+					{
+						if (Convert.ChangeType(match.Groups[2].Value, GetType<T2>(), CultureInfo.InvariantCulture) is T2 value2)
+						{
+							if (Convert.ChangeType(match.Groups[3].Value, GetType<T3>(), CultureInfo.InvariantCulture) is T3 value3)
+							{
+								return combiner(value1, value2, value3);
+							}
+							else
+							{
+								throw new FormatException($"Could not convert group 3 (\"{match.Groups[3].Value}\") to {GetType<T3>().Name}. From \"{input}\".");
+							}
+						}
+						else
+						{
+							throw new FormatException($"Could not convert group 2 (\"{match.Groups[2].Value}\") to {GetType<T2>().Name}. From \"{input}\".");
+						}
+					}
+					else
+					{
+						throw new FormatException($"Could not convert group 1 (\"{match.Groups[1].Value}\") to {GetType<T1>().Name}. From \"{input}\".");
+					}
+				}
+				else
+				{
+					return default;
+				}
+			};
+		}
+
+		public static Func<string, (T1, T2, T3, T4)?> PatternParser<T1, T2, T3, T4>(string pattern) =>
+			PatternParser<T1, T2, T3, T4, (T1, T2, T3, T4)?>(new Regex(pattern), static (v1, v2, v3, v4) => (v1, v2, v3, v4));
+		public static Func<string, TO?> PatternParser<T1, T2, T3, T4, TO>(string pattern, Func<T1, T2, T3, T4, TO> combiner) =>
+			PatternParser<T1, T2, T3, T4, TO>(new Regex(pattern), combiner);
+		public static Func<string, (T1, T2, T3, T4)?> PatternParser<T1, T2, T3, T4>(Regex pattern) =>
+			PatternParser<T1, T2, T3, T4, (T1, T2, T3, T4)?>(pattern, static (v1, v2, v3, v4) => (v1, v2, v3, v4));
+		public static Func<string, TO?> PatternParser<T1, T2, T3, T4, TO>(Regex pattern, Func<T1, T2, T3, T4, TO> combiner)
+		{
+			if (pattern.GetGroupNumbers().Length != 5)
+			{
+				throw new ArgumentException("Pattern must have exactly four groups.", nameof(pattern));
+			}
+
+			return input =>
+			{
+				Match match = pattern.Match(input);
+				if (match.Success && match.Groups[1].Success && match.Groups[2].Success &&
+					match.Groups[3].Success && match.Groups[4].Success)
+				{
+					if (Convert.ChangeType(match.Groups[1].Value, GetType<T1>(), CultureInfo.InvariantCulture) is T1 value1)
+					{
+						if (Convert.ChangeType(match.Groups[2].Value, GetType<T2>(), CultureInfo.InvariantCulture) is T2 value2)
+						{
+							if (Convert.ChangeType(match.Groups[3].Value, GetType<T3>(), CultureInfo.InvariantCulture) is T3 value3)
+							{
+								if (Convert.ChangeType(match.Groups[4].Value, GetType<T4>(), CultureInfo.InvariantCulture) is T4 value4)
+								{
+									return combiner(value1, value2, value3, value4);
+								}
+								else
+								{
+									throw new FormatException($"Could not convert group 4 (\"{match.Groups[4].Value}\") to {GetType<T4>().Name}. From \"{input}\".");
+								}
+							}
+							else
+							{
+								throw new FormatException($"Could not convert group 3 (\"{match.Groups[3].Value}\") to {GetType<T3>().Name}. From \"{input}\".");
+							}
+						}
+						else
+						{
+							throw new FormatException($"Could not convert group 2 (\"{match.Groups[2].Value}\") to {GetType<T2>().Name}. From \"{input}\".");
+						}
+					}
+					else
+					{
+						throw new FormatException($"Could not convert group 1 (\"{match.Groups[1].Value}\") to {GetType<T1>().Name}. From \"{input}\".");
+					}
+				}
+				else
+				{
+					return default;
+				}
+			};
+		}
+
+		private static Type GetType<T>()
+		{
+			Type t = typeof(T);
+			if (t.Name == "Nullable`1")
+			{
+				t = t.GenericTypeArguments[0];
+			}
+			return t;
+		}
+	}
+}
---
+++ test/Parsing.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AoC.Library.Test
+{
+	[TestClass]
+	public class ParsingTest
+	{
+		[TestMethod]
+		public void PatternParser1_MatchAndNotMatch()
+		{
+			Func<string, double?> heightParser = Parsing.PatternParser<double?>(@"height: (\d+(?:\.\d+)?)");
+			Assert.AreEqual(1.93, heightParser("height: 1.93m"));
+			Assert.IsNull(heightParser("age: 22 years"));
+		}
+
+		[TestMethod]
+		public void PatternParser1_Select()
+		{
+			IEnumerable<string> lines = new string[]
+			{
+				"height: 1.93m",
+				"height: 2.18m",
+				"height: 2.08m",
+			};
+			Assert.IsTrue(
+				lines.Select(Parsing.PatternParser<double>(@"height: (\d+(?:\.\d+)?)"))
+					.SequenceEqual(new double[]
+					{
+						1.93d,
+						2.18d,
+						2.08d,
+					})
+			);
+		}
+
+		[TestMethod]
+		public void PatternParser2_MatchAndNotMatch()
+		{
+			Func<string, (string, int)?> nameAgeParser = Parsing.PatternParser<string, int>(@"name: ([^,]+), age: (\d+)");
+			Assert.AreEqual(("Mårten Åsberg", 22), nameAgeParser("name: Mårten Åsberg, age: 22"));
+			Assert.IsNull(nameAgeParser("height: 1.93m"));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void PatternParser2_GroupMismatch()
+		{
+			_ = Parsing.PatternParser<string, int>(@"name: ([^,]+)");
+		}
+
+		[TestMethod]
+		public void PatternParser3_MatchAndNotMatch()
+		{
+			Func<string, (string, int, double)?> nameAgeHeightParser = Parsing.PatternParser<string, int, double>(@"name: ([^,]+), age: (\d+), height: (\d+(?:\.\d+))");
+			Assert.AreEqual(("Mårten Åsberg", 22, 1.93), nameAgeHeightParser("name: Mårten Åsberg, age: 22, height: 1.93"));
+			Assert.IsNull(nameAgeHeightParser("name: Mårten Åsberg, height: 1.93, age: 22"));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(FormatException))]
+		public void PatternParser3_GroupMismatch()
+		{
+			Func<string, (string, int, double)?> nameAgeHeightParser = Parsing.PatternParser<string, int, double>(@"name: ([^,]+), age: ([^,]+), height: ([^,]+)");
+			_ = nameAgeHeightParser("name: Mårten Åsberg, age: twenty-two, height: 1.93");
+		}
+
+		[TestMethod]
+		public void PatternParser4_MatchAndNotMatch()
+		{
+			Func<string, (string, int, double, bool)?> nameAgeHeightIsAliveParser = Parsing.PatternParser<string, int, double, bool>(@"name: ([^,]+), age: (\d+), height: (\d+(?:\.\d+)), is alive: (true|false)");
+			Assert.AreEqual(("Mårten Åsberg", 22, 1.93, true), nameAgeHeightIsAliveParser("name: Mårten Åsberg, age: 22, height: 1.93, is alive: true"));
+			Assert.IsNull(nameAgeHeightIsAliveParser("name: Mårten Åsberg, height: 1.93, age: 22, is alive: false"));
+		}
+
+		record Person(string name, int age, double height, bool isAlive);
+		static Person MakePerson(string name, int age, double height, bool isAlive) =>
+			new Person(name, age, height, isAlive);
+		[TestMethod]
+		public void PatternParser4_Combiner()
+		{
+			Func<string, Person?> personParser = Parsing.PatternParser<string, int, double, bool, Person>(@"name: ([^,]+), age: (\d+), height: (\d+(?:\.\d+)), is alive: (true|false)", MakePerson);
+			Assert.AreEqual(new Person("Mårten Åsberg", 22, 1.93, true), personParser("name: Mårten Åsberg, age: 22, height: 1.93, is alive: true"));
+			Assert.IsNull(personParser("name: Mårten Åsberg, height: 1.93, age: 22, is alive: false"));
+		}
+	}
+}
