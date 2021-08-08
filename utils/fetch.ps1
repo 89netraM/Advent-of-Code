d8e69620afc97de76560c259d8f3daa7e76bfd9a
Mårten Åsberg <marten.asberg@outlook.com>
2021-08-08 17:02:20 +0200

    No extra new lines in input files from fetch util

--- utils/fetch.ps1
+++ utils/fetch.ps1
@@ -32,7 +32,7 @@ try {
 	$headers = @{ "cookie" = (Get-Content "$basepath\utils\aoccookie") };
 
 	try {
-		(Invoke-WebRequest -Headers $headers -Uri "https://adventofcode.com/$Year/day/$Day/input").Content | Out-File -Path "$dayPath\input.txt";
+		(Invoke-WebRequest -Headers $headers -Uri "https://adventofcode.com/$Year/day/$Day/input").Content | Out-File -Path "$dayPath\input.txt" -NoNewline;
 
 		$sourceFile = "$dayPath\Day$Day.$Language";
 
