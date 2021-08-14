be0ee336ea16cdf92da27de66e293d8a551b3bc9
Mårten Åsberg <marten.asberg@outlook.com>
2021-08-14 16:41:18 +0200

    2017 Day 20
    Part 2
    It wasn't even the maths fault! It was mine, I didn't read the
    description thoroughly enough!

--- 2017/20/Day20.cs
+++ 2017/20/Day20.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AoC.Library;
 
@@ -33,6 +34,56 @@ namespace AoC.Year2017
 			}
 
 			return particles.Select(static (p, i) => (p, i)).Aggregate(static (a, b) => Vector3.Zero.ManhattanDistance(a.p.pos) < Vector3.Zero.ManhattanDistance(b.p.pos) ? a : b).i;
+		}
+
+		[Part(2)]
+		public object Part2(string input)
+		{
+			var particles = input.Lines()
+				.Select(Parsing.PatternParser<string, string, string, Particle>(
+					@"p=<(-?\d+,-?\d+,-?\d+)>, v=<(-?\d+,-?\d+,-?\d+)>, a=<(-?\d+,-?\d+,-?\d+)>",
+					static (p, v, a) => new Particle(vectorParser(p), vectorParser(v), vectorParser(a))
+				))
+				.Select(static (p, i) => (p, i))
+				.ToDictionary(static p => p.i, static p => p.p);
+
+			var duplicates = new Dictionary<Vector3, IList<int>>();
+
+			for (int steps = 0; steps < 1000; steps++)
+			{
+				foreach (int id in particles.Keys)
+				{
+					var particle = particles[id];
+					var nextVel = particle.vel + particle.acc;
+					var nextPos = particle.pos + nextVel;
+					particles[id] = particle with {
+						pos = nextPos,
+						vel = nextVel,
+					};
+
+					IList<int> ids;
+					if (!duplicates.TryGetValue(nextPos, out ids))
+					{
+						ids = new List<int>();
+						duplicates.Add(nextPos, ids);
+					}
+					ids.Add(id);
+				}
+
+				foreach (var ids in duplicates.Values)
+				{
+					if (ids.Count > 1)
+					{
+						foreach (var id in ids)
+						{
+							particles.Remove(id);
+						}
+					}
+				}
+				duplicates.Clear();
+			}
+
+			return particles.Count;
 		}
 	}
 }
