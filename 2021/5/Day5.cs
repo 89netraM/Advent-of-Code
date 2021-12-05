293f49dced0f1fb83c8e0c4d656f8c90b85d17d8
Mårten Åsberg <marten.asberg@outlook.com>
2021-12-05 06:37:09 +0100

    Using the RegExtract library
    Much nicer than regular RegEx and better working than my Parsing
    library.

--- 2021/5/Day5.cs
+++ 2021/5/Day5.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using AoC.Library;
+using RegExtract;
 
 namespace AoC.Year2021
 {
@@ -12,39 +12,30 @@ namespace AoC.Year2021
 		[Part(1)]
 		public object Part1(string input)
 		{
-			Regex regex = new Regex(@"(\d+),(\d+) -> (\d+),(\d+)");
 			Dictionary<Vector2, long> map = new Dictionary<Vector2, long>();
 			foreach (var line in input.Lines())
 			{
-				var m = regex.Match(line);
-				if (m.Success)
+				var from = line.Extract<Vector2>(@"(\d+),(\d+) ->");
+				var to = line.Extract<Vector2>(@"-> (\d+),(\d+)");
+				if (from.X == to.X)
 				{
-					Vector2 from = new Vector2(long.Parse(m.Groups[1].Value), long.Parse(m.Groups[2].Value));
-					Vector2 to = new Vector2(long.Parse(m.Groups[3].Value), long.Parse(m.Groups[4].Value));
-					if (from.X == to.X)
+					var f = from.MinParts(to);
+					var t = from.MaxParts(to);
+					for (long y = f.Y; y <= t.Y; y++)
 					{
-						var f = from.MinParts(to);
-						var t = from.MaxParts(to);
-						for (long y = f.Y; y <= t.Y; y++)
-						{
-							var pos = new Vector2(f.X, y);
-							if (map.ContainsKey(pos)) { map[pos]++; } else { map.Add(pos, 1); }
-						}
-					}
-					else if (from.Y == to.Y)
-					{
-						var f = from.MinParts(to);
-						var t = from.MaxParts(to);
-						for (long x = f.X; x <= t.X; x++)
-						{
-							var pos = new Vector2(x, f.Y);
-							if (map.ContainsKey(pos)) { map[pos]++; } else { map.Add(pos, 1); }
-						}
+						var pos = new Vector2(f.X, y);
+						if (map.ContainsKey(pos)) { map[pos]++; } else { map.Add(pos, 1); }
 					}
 				}
-				else
+				else if (from.Y == to.Y)
 				{
-					Console.WriteLine($"Unable to parse line: {line}");
+					var f = from.MinParts(to);
+					var t = from.MaxParts(to);
+					for (long x = f.X; x <= t.X; x++)
+					{
+						var pos = new Vector2(x, f.Y);
+						if (map.ContainsKey(pos)) { map[pos]++; } else { map.Add(pos, 1); }
+					}
 				}
 			}
 			return map.Count(kvp => kvp.Value >= 2);
@@ -53,29 +44,20 @@ namespace AoC.Year2021
 		[Part(2)]
 		public object Part2(string input)
 		{
-			Regex regex = new Regex(@"(\d+),(\d+) -> (\d+),(\d+)");
 			Dictionary<Vector2, long> map = new Dictionary<Vector2, long>();
 			foreach (var line in input.Lines())
 			{
-				var m = regex.Match(line);
-				if (m.Success)
-				{
-					Vector2 from = new Vector2(long.Parse(m.Groups[1].Value), long.Parse(m.Groups[2].Value));
-					Vector2 to = new Vector2(long.Parse(m.Groups[3].Value), long.Parse(m.Groups[4].Value));
+				var from = line.Extract<Vector2>(@"(\d+),(\d+) ->");
+				var to = line.Extract<Vector2>(@"-> (\d+),(\d+)");
 
-					Vector2 step = to - from;
-					step = new Vector2(step.X == 0 ? 0 : 1 * Math.Sign(step.X), step.Y == 0 ? 0 : 1 * Math.Sign(step.Y));
-					while (from != to)
-					{
-						if (map.ContainsKey(from)) { map[from]++; } else { map.Add(from, 1); }
-						from += step;
-					}
-					if (map.ContainsKey(from)) { map[from]++; } else { map.Add(from, 1); }
-				}
-				else
+				Vector2 step = to - from;
+				step = new Vector2(step.X == 0 ? 0 : 1 * Math.Sign(step.X), step.Y == 0 ? 0 : 1 * Math.Sign(step.Y));
+				while (from != to)
 				{
-					Console.WriteLine($"Unable to parse line: {line}");
+					if (map.ContainsKey(from)) { map[from]++; } else { map.Add(from, 1); }
+					from += step;
 				}
+				if (map.ContainsKey(from)) { map[from]++; } else { map.Add(from, 1); }
 			}
 			return map.Count(kvp => kvp.Value >= 2);
 		}
