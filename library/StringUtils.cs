9376957e8d0d320f466fd6f1b0b720e117860b6f
Mårten Åsberg <marten.asberg@outlook.com>
2021-12-15 06:58:23 +0100

    Library function creating maps from char blocks

--- library/StringUtils.cs
+++ library/StringUtils.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using static AoC.Library.Functional;
 
 namespace AoC.Library
 {
@@ -12,5 +15,23 @@ namespace AoC.Library
 
 		public static string[] Words(this string s) =>
 			s.Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+		/// <summary>
+		/// Transforms a string into a <see cref="Vector2"/> map of characters.
+		/// </summary>
+		public static Dictionary<Vector2, char> ToMap(this string s) =>
+			s.ToMap(Id);
+		/// <summary>
+		/// Transforms a string into a <see cref="Vector2"/> map of longs, assuming the characters are ASCII numbers.
+		/// </summary>
+		public static Dictionary<Vector2, long> ToMapLong(this string s) =>
+			s.ToMap(static c => (long)(c - '0'));
+		/// <summary>
+		/// Transforms a string into a <see cref="Vector2"/> map where values are built by the <paramref name="transformer"/>.
+		/// </summary>
+		public static Dictionary<Vector2, T> ToMap<T>(this string s, Func<char, T> transformer) =>
+			s.Lines()
+				.SelectMany((l, y) => l.Select((c, x) => (x, y, c)))
+				.ToDictionary(t => new Vector2(t.x, t.y), t => transformer(t.c));
 	}
 }
