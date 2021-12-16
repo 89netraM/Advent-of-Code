551260fb8577616c2f751a1e1750fed76f6e3c81
Mårten Åsberg <marten.asberg@outlook.com>
2021-12-16 19:03:55 +0100

    2021 Day 16, but better
    Prettier, more reusable code. For when we eventually have to parse this
    format for another day.

--- 2021/16/Day16.cs
+++ 2021/16/Day16.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AoC.Library;
-using static AoC.Library.Functional;
+using OneOf;
 
 namespace AoC.Year2021
 {
@@ -12,100 +12,158 @@ namespace AoC.Year2021
 		[Part(1)]
 		public object Part1(string input)
 		{
-			IReadOnlyList<bool> bits = input.SelectMany(static c => Convert.ToString(Convert.ToByte(c.ToString(), 16), 2).PadLeft(4, '0')).Select(static c => c == '1').ToArray();
-
-			long versionSum = 0L;
-			for (int i = 0; bits.Skip(i).Any(Id);)
-			{
-				var version = GetBits(bits, ref i, 3);
-				versionSum += version;
-				var type = GetBits(bits, ref i, 3);
-				if (type == 4L)
-				{
-					long part = 0b10000L;
-					while ((part & 0b10000L) != 0L)
-					{
-						part = GetBits(bits, ref i, 5);
-					}
-				}
-				else
-				{
-					var lengthType = GetBits(bits, ref i, 1);
-					i += lengthType == 0L ? 15 : 11;
-				}
-			}
-			return versionSum;
+			static long Visit(BITS bits) =>
+				bits.Version + bits.Value.Match(
+					_ => 0L,
+					children => children.Sum(Visit)
+				);
+			return Visit(BITS.Parse(input));
 		}
 
 		[Part(2)]
 		public object Part2(string input)
 		{
-			IReadOnlyList<bool> bits = input.SelectMany(static c => Convert.ToString(Convert.ToByte(c.ToString(), 16), 2).PadLeft(4, '0')).Select(static c => c == '1').ToArray();
-			int i = 0;
-			return EvaluatePackate(bits, ref i);
+			static long Evaluate(BITS node) =>
+				node.Value.Match(
+					v => v,
+					children => node.Type switch
+					{
+						0 => children.Sum(Evaluate),
+						1 => children.Aggregate(1L, static (p, n) => p * Evaluate(n)),
+						2 => children.Min(Evaluate),
+						3 => children.Max(Evaluate),
+						5 => Evaluate(children[0]) > Evaluate(children[1]) ? 1L : 0L,
+						6 => Evaluate(children[0]) < Evaluate(children[1]) ? 1L : 0L,
+						7 => Evaluate(children[0]) == Evaluate(children[1]) ? 1L : 0L,
+						_ => throw new InvalidOperationException(),
+					}
+				);
+			return Evaluate(BITS.Parse(input));
+		}
+	}
+
+	public class BITS
+	{
+		/// <summary>
+		/// Turns a hexadecimal string into a sequence of bits. Each bit is represented by a long 1 or 0.
+		/// </summary>
+		public static IEnumerable<long> ParseBitStream(string input) =>
+			input.SelectMany(static c =>
+			{
+				long b = ((long)c & 0b1111L) + (c > '9' ? 9L : 0L);
+				return new long[] { (b & 0b1000L) >> 3, (b & 0b0100L) >> 2, (b & 0b0010L) >> 1, b & 0b0001L };
+			});
+
+		/// <summary>
+		/// Turns a sequence of bits into a tree of BITS nodes.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="versionWidth">The number of bits that represent the version number.</param>
+		/// <param name="typeWidth">The number of bits that represent the type number</param>
+		/// <param name="literalType">The type number of packets that contain literal values, as opposed to a sequence of child packets.</param>
+		/// <param name="literalChunkWidth">The number of bits in a chunk of literal values.</param>
+		/// <param name="childBitCountWidth">The number of bits that represent the bit-count of children.</param>
+		/// <param name="childCountWidth">The number of bits that represent number of children.</param>
+		/// <inheritdoc cref="InternalParse"/>
+		public static BITS Parse(string input, int versionWidth = 3, int typeWidth = 3, long literalType = 4L, int literalChunkWidth = 5, int childBitCountWidth = 15, int childCountWidth = 11)
+		{
+			long literalChunkMask = 0L;
+			for (int i = 0; i < literalChunkWidth - 1; i++)
+			{
+				literalChunkMask = literalChunkMask << 1 | 0b1L;
+			}
+
+			using var bits = ParseBitStream(input).GetEnumerator();
+			return InternalParse(bits, versionWidth, typeWidth, literalType, literalChunkWidth, literalChunkMask, childBitCountWidth, childCountWidth).packet;
 		}
 
-		private long EvaluatePackate(IReadOnlyList<bool> bits, ref int i)
+		/// <inheritdoc cref="Next"/>
+		private static (BITS packet, long length) InternalParse(IEnumerator<long> bits, int versionWidth, int typeWidth, long literalType, int literalChunkWidth, long literalChunkMask, int childBitCountWidth, int childCountWidth)
 		{
-			var version = GetBits(bits, ref i, 3);
-			var type = GetBits(bits, ref i, 3);
-			if (type == 4L)
+			long version = Next(bits, versionWidth);
+			long type = Next(bits, typeWidth);
+			long length = versionWidth + typeWidth;
+			if (type == literalType)
 			{
-				long part = 0b10000L;
+				long continueMask = 0b1L << (literalChunkWidth - 1);
+				long part = continueMask;
 				long value = 0L;
-				while ((part & 0b10000L) != 0L)
+				while ((part & continueMask) != 0L)
 				{
-					part = GetBits(bits, ref i, 5);
-					value = (value << 4) | (part & 0b1111L);
+					part = Next(bits, literalChunkWidth);
+					length += literalChunkWidth;
+					value = (value << (literalChunkWidth - 1)) | (part & literalChunkMask);
 				}
-				return value;
+				return (new BITS(version, type, value), length);
 			}
 			else
 			{
-				var lengthType = GetBits(bits, ref i, 1);
-				List<long> subValues;
+				long lengthType = Next(bits);
+				length += 1;
+				List<BITS> children = new List<BITS>();
 				if (lengthType == 0L)
 				{
-					long bitCount = GetBits(bits, ref i, 15);
-					int start = i;
-					subValues = new List<long>();
-					while (i - start < bitCount)
+					long bitCount = Next(bits, childBitCountWidth);
+					length += childBitCountWidth;
+					long start = length;
+					while (length - start < bitCount)
 					{
-						subValues.Add(EvaluatePackate(bits, ref i));
+						var (child, childLength) = InternalParse(bits, versionWidth, typeWidth, literalType, literalChunkWidth, literalChunkMask, childBitCountWidth, childCountWidth);
+						length += childLength;
+						children.Add(child);
 					}
 				}
 				else
 				{
-					int packetCount = (int)GetBits(bits, ref i, 11);
-					subValues = new List<long>(packetCount);
-					for (int j = 0; j < packetCount; j++)
+					long packetCount = Next(bits, childCountWidth);
+					length += childCountWidth;
+					for (long i = 0; i < packetCount; i++)
 					{
-						subValues.Add(EvaluatePackate(bits, ref i));
+						var (child, childLength) = InternalParse(bits, versionWidth, typeWidth, literalType, literalChunkWidth, literalChunkMask, childBitCountWidth, childCountWidth);
+						length += childLength;
+						children.Add(child);
 					}
 				}
-				return type switch
-				{
-					0L => subValues.Sum(),
-					1L => subValues.Aggregate(1L, static (a, b) => a * b),
-					2L => subValues.Min(),
-					3L => subValues.Max(),
-					5L => subValues[0] > subValues[1] ? 1L : 0L,
-					6L => subValues[0] < subValues[1] ? 1L : 0L,
-					7L => subValues[0] == subValues[1] ? 1L : 0L,
-					_ => throw new Exception($"Invalid type {type}"),
-				};
+				return (new BITS(version, type, children), length);
 			}
-			throw new Exception("Malformatted packet");
 		}
 
-		private long GetBits(IReadOnlyList<bool> bits, ref int i, int count)
+		/// <summary>
+		/// Moves and returns the next bit of the enumerator, or throws an exception if the enumerator is exhausted.
+		/// </summary>
+		/// <param name="bits">The enumerator to move.</param>
+		/// <exception cref="InvalidOperationException">The enumerator is exhausted.</exception>
+		public static long Next(IEnumerator<long> bits) =>
+			bits.MoveNext() ? bits.Current : throw new InvalidOperationException("Unexpected end of stream");
+		/// <summary>
+		/// Moves the enumerator and returns the value represented by the consumed bits, or throws an exception if the enumerator is exhausted.
+		/// </summary>
+		/// <param name="count">The number of bits to consume.</param>
+		/// <inheritdoc cref="Next"/>
+		public static long Next(IEnumerator<long> bits, int count)
 		{
 			long result = 0L;
-			for (int j = 0; j < count; j++)
+			for (int i = 0; i < count; i++)
 			{
-				result = (result << 1) | (bits[i++] ? 1L : 0L);
+				result = (result << 1) | Next(bits);
 			}
 			return result;
 		}
+
+		/// <summary>
+		/// The version of the packet.
+		/// </summary>
+		public long Version { get; }
+		/// <summary>
+		/// The type of the packet.
+		/// </summary>
+		public long Type { get; }
+		/// <summary>
+		/// Either the literal value of the packet, or a list of the packets children.
+		/// </summary>
+		public OneOf<long, IReadOnlyList<BITS>> Value { get; }
+
+		public BITS(long version, long type, OneOf<long, IReadOnlyList<BITS>> value) =>
+			(Version, Type, Value) = (version, type, value);
 	}
 }
