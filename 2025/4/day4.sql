741f1c75fb0fd1db9170c4c8e25930df544434c5
Mårten Åsberg <marten.asberg@outlook.com>
2025-12-04 16:45:16 +0100

    2025 Day 4 Part 2 WIP
    It's super slow! I part two takes over half an hour.

--- 2025/4/day4.sql
+++ 2025/4/day4.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Input(line TEXT, lineNumber INTEGER PRIMARY KEY AUTOINCREMENT);
 .import input.txt Input
 
-CREATE TABLE Map(x INTEGER, y INTEGER, neighbors INTEGER, PRIMARY KEY (x, y));
+CREATE TABLE Map(x INTEGER, y INTEGER, PRIMARY KEY (x, y));
 WITH RECURSIVE Splitter AS (
    SELECT
        lineNumber as y,
@@ -18,12 +18,9 @@ WITH RECURSIVE Splitter AS (
    FROM Splitter
    WHERE remainder != ''
 )
-INSERT INTO Map SELECT x, y, 0 AS neighbors FROM Splitter WHERE part = '@';
+INSERT INTO Map SELECT x, y FROM Splitter WHERE part = '@';
 
-UPDATE
-    Map
-SET
-    neighbors = (
+SELECT 'Part 1: ' || (SELECT count(*) FROM (SELECT (
         SELECT
             count(*)
         FROM
@@ -32,6 +29,50 @@ SET
             (Map2.x = Map.x OR Map2.x + 1 = Map.x OR Map2.x - 1 = Map.x)
             AND (Map2.y = Map.y OR Map2.y + 1 = Map.y OR Map2.y - 1 = Map.y)
             AND NOT (Map2.x = Map.x AND Map2.y = Map.y)
-    );
+    ) AS neighbors FROM Map) WHERE neighbors < 4);
 
-SELECT 'Part 1: ' || (SELECT count(*) FROM Map WHERE neighbors < 4);
+WITH Iterations AS (
+    WITH RECURSIVE Forklift AS (
+        SELECT
+            1 as iteration,
+            (
+                SELECT json_group_array(json_object('x', Map.x, 'y', Map.y))
+                FROM Map
+                WHERE
+                (
+                    SELECT count(*)
+                    FROM Map Map2
+                    WHERE
+                        (Map2.x = Map.x OR Map2.x + 1 = Map.x OR Map2.x - 1 = Map.x)
+                        AND (Map2.y = Map.y OR Map2.y + 1 = Map.y OR Map2.y - 1 = Map.y)
+                        AND NOT (Map2.x = Map.x AND Map2.y = Map.y)
+                ) >= 4
+            ) AS paperRolls
+        UNION ALL
+        SELECT
+            iteration + 1 AS iteration,
+            (
+                WITH ExistingPaperRolls AS (
+                    SELECT
+                        json_extract(value, '$.x') AS x,
+                        json_extract(value, '$.y') AS y
+                    FROM json_each(paperRolls)
+                )
+                SELECT json_group_array(json_object('x', p.x, 'y', p.y))
+                FROM ExistingPaperRolls p
+                WHERE
+                (
+                    SELECT count(*)
+                    FROM ExistingPaperRolls p2
+                    WHERE
+                        (p2.x = p.x OR p2.x + 1 = p.x OR p2.x - 1 = p.x)
+                        AND (p2.y = p.y OR p2.y + 1 = p.y OR p2.y - 1 = p.y)
+                        AND NOT (p2.x = p.x AND p2.y = p.y)
+                ) >= 4
+            ) AS nextPaperRolls
+        FROM Forklift
+        WHERE nextPaperRolls != paperRolls
+    )
+    SELECT max(iteration), json_array_length(paperRolls, '$') AS remainingPaperRolls FROM Forklift
+)
+SELECT 'Part 2: ' || (SELECT (SELECT count(*) FROM Map) - (SELECT remainingPaperRolls FROM Iterations));
