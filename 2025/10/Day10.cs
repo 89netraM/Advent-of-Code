7e9a2cb980919e4b5892f2582bbda5a2ebfa5631
Mårten Åsberg <marten.asberg@outlook.com>
2025-12-12 08:44:45 +0100

    2025 Day 10
    It should have worked in Matlab!
    
    Also, I think there's an easier solution to part 1...
    My part 2 is easier than my part 1 🙂

--- 2025/10/Day10.cs
+++ 2025/10/Day10.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using AoC.Library;
 using Microsoft.Z3;
 using RegExtract;
-using static AoC.Library.Functional;
 
 namespace AoC.Year2025;
 
@@ -13,13 +11,84 @@ namespace AoC.Year2025;
 public class Day10
 {
 	[Part(1)]
-	public object Part1(string input) => Parse(input).AsParallel().Sum(Solve);
+	public object Part1(string input) =>
+		Parse(input)
+			.AsParallel()
+			.Sum(manual =>
+			{
+				using var ctx = new Context();
+				var optimizer = ctx.MkOptimize();
+				var buttons = manual
+					.Buttons.Select(
+						(_, i) =>
+						{
+							var button = ctx.MkIntConst($"button_{i}");
+							optimizer.Assert(button >= 0);
+							return button;
+						}
+					)
+					.ToArray();
+				var lights = manual
+					.Lights.Select(
+						(_, i) =>
+						{
+							var light = ctx.MkIntConst($"light_{i}");
+							optimizer.Assert(light >= 0);
+							return light;
+						}
+					)
+					.ToArray();
+				for (var i = 0; i < manual.Lights.Length; i++)
+				{
+					var equation =
+						2 * lights[i]
+						- manual
+							.Buttons.Select((b, i) => (b, i))
+							.Where(p => p.b.Contains(i))
+							.Select(p => buttons[p.i])
+							.Aggregate<ArithExpr>((a, b) => a + b);
+					optimizer.Assert(ctx.MkEq(equation, ctx.MkInt(manual.Lights[i] ? 1 : 0)));
+				}
+				var buttonCount = buttons.Aggregate<ArithExpr>((a, b) => a + b);
+				optimizer.MkMinimize(buttonCount);
+				return optimizer.Check() is Status.SATISFIABLE
+					? ((IntNum)optimizer.Model.Eval(buttonCount)).Int64
+					: throw new Exception("Not satisfiable.");
+			});
 
 	[Part(2)]
-	public object Part2(string input)
-	{
-		return null;
-	}
+	public object Part2(string input) =>
+		Parse(input)
+			.AsParallel()
+			.Sum(manual =>
+			{
+				using var ctx = new Context();
+				var optimizer = ctx.MkOptimize();
+				var buttons = manual
+					.Buttons.Select(
+						(_, i) =>
+						{
+							var button = ctx.MkIntConst($"button_{i}");
+							optimizer.Assert(button >= 0);
+							return button;
+						}
+					)
+					.ToArray();
+				for (var i = 0; i < manual.Lights.Length; i++)
+				{
+					var equation = manual
+						.Buttons.Select((b, i) => (b, i))
+						.Where(p => p.b.Contains(i))
+						.Select(p => buttons[p.i])
+						.Aggregate<ArithExpr>((a, b) => a + b);
+					optimizer.Assert(ctx.MkEq(equation, ctx.MkInt(manual.Joltages[i])));
+				}
+				var buttonCount = buttons.Aggregate<ArithExpr>((a, b) => a + b);
+				optimizer.MkMinimize(buttonCount);
+				return optimizer.Check() is Status.SATISFIABLE
+					? ((IntNum)optimizer.Model.Eval(buttonCount)).Int64
+					: throw new Exception("Not satisfiable.");
+			});
 
 	private static IEnumerable<Manual> Parse(string input) =>
 		input
@@ -30,26 +99,6 @@ public class Day10
 				[.. t.Item2.Words().Select(b => b[1..^1].Split(',').Select(int.Parse).ToArray())],
 				[.. t.Item3.Split(',').Select(int.Parse)]
 			));
-
-	private static long Solve(Manual manual)
-	{
-		using var ctx = new Context();
-		var optimizer = ctx.MkOptimize();
-		var buttons = manual.Buttons.Select((_, i) => ctx.MkIntConst($"button_{i}")).ToArray();
-		for (var i = 0; i < manual.Lights.Length; i++)
-		{
-			var equation = manual
-				.Buttons.Select((b, i) => (b, i))
-				.Where(p => p.b.Contains(i))
-				.Select(p => -buttons[p.i] + 2 * ctx.MkIntConst($"button_diff_{i}"))
-				.Aggregate((a, b) => a + b);
-			optimizer.Assert(ctx.MkEq(equation, ctx.MkInt(manual.Lights[i] ? 1 : 0)));
-		}
-		optimizer.MkMinimize(buttons.Aggregate<ArithExpr>((a, b) => a + b));
-		return optimizer.Check() is Status.SATISFIABLE
-			? ((IntNum)optimizer.Model.Eval(optimizer.Objectives[0])).Int64
-			: throw new Exception("Not satisfiable.");
-	}
 
 	private record Manual(bool[] Lights, int[][] Buttons, int[] Joltages);
 }
